d6c8f4ec85e9952d213f79060ea975c5ccd7481f
Joao Lino <linusmannuel@gmail.com>
2023-05-06 22:04:37 +0100

    refator main js

--- src/frontend/script/main.js
+++ src/frontend/script/main.js
@@ -1,8 +1,8 @@
 // import { openMenu } from '/components/OpenMenu.js';
 
 function openMenu() {
-	const btnHamburger = document.querySelector('.c-btn--hamburger');
-	const menu = document.querySelector('.c-menu__navigation');
+	const btnHamburger = document.querySelector('[data-openMenu]');
+	const menu = document.querySelector('[data-menu]');
 
 	btnHamburger.addEventListener('click', () => {
 		btnHamburger.classList.toggle('active');
