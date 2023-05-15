4294ca57fdea22dff03e573a32907bdbf2c8700c
Joao Lino <linusmannuel@gmail.com>
2023-05-15 22:50:18 +0100

    add function card active

--- src/frontend/script/main.js
+++ src/frontend/script/main.js
@@ -1,5 +1,4 @@
-// import { openMenu } from '/components/OpenMenu.js';
-
+//Essas funções vão ficar em arquivos separados no futuro...
 function openMenu() {
 	const btnHamburger = document.querySelector('[data-openMenu]');
 	const menu = document.querySelector('[data-menu]');
@@ -10,4 +9,15 @@ function openMenu() {
 	});
 }
 
+function activeCard() {
+	const btnPlus = document.querySelector('[data-active]');
+	const cardEmployees = document.querySelector('[data-employees]');
+
+	btnPlus.addEventListener('click', () => {
+		cardEmployees.classList.toggle('active');
+		btnPlus.classList.toggle('active');
+	});
+}
+
+activeCard();
 openMenu();
